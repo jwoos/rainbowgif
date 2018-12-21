aef5f7a4c10bd5019c1bb6ec6ce7c7306badcabf
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-12-21 03:15:46 -0500

    working with gifs

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
 
-import "github.com/lucasb-eyer/go-colorful"
+import (
+	"fmt"
+	"image/gif"
+	//"io/ioutil"
+	"os"
+
+	//"github.com/lucasb-eyer/go-colorful"
+)
 
 
 func main() {
+	file, err := os.Open("test.gif")
+	if (err != nil) {
+		fmt.Println("Error opening file: ", err)
+		os.Exit(1)
+	}
+
+	decoded, err := gif.DecodeAll(file)
+	if (err != nil) {
+		fmt.Println("Error decoding: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(decoded)
+
+
+	for i, frame := range decoded.Image {
+		frame := decode.Image[i]
+		delay := decode.Delay[i]
+	}
 }
