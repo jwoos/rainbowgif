b11c21db87db1e4e27245142ca0a15af096d15c4
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-12-28 00:25:35 -0500

    sort of works?

--- main.go
+++ main.go
@@ -3,51 +3,84 @@ package main
 
 import (
 	"fmt"
-	//"image"
-	//"image/color"
+	"image/color"
 	"image/gif"
-	//"io/ioutil"
 	"os"
 
-	//"github.com/lucasb-eyer/go-colorful"
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 
 func main() {
 	file, err := os.Open("test.gif")
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error opening file: ", err)
 		os.Exit(1)
 	}
 
-	originalImage, err := gif.DecodeAll(file)
-	if (err != nil) {
+	image, err := gif.DecodeAll(file)
+	if err != nil {
 		fmt.Println("Error decoding: ", err)
 		os.Exit(1)
 	}
 
-	frameCount := len(originalImage.Image)
-	fmt.Println(frameCount)
+	frameCount := len(image.Image)
+
+	// ROYGBV
+	gradient := newGradient(
+		[]colorful.Color{
+			{1, 0, 0},
+			{1, 127.0 / 255.0, 0},
+			{1, 1, 0},
+			{0, 1, 0},
+			{0, 0, 1},
+			{139.0 / 255.0, 0, 1},
+		},
+	)
+	overlayColors := gradient.generate(frameCount)
 
 	/* frame is just `*image.Paletted`
 	 * `color.Palette` is just `[]color.Color`
-	 * `[]color.Color` is an interface implementing `RGBA()`
+	 * `color.Color` is an interface implementing `RGBA()`
 	 */
-	/*
-	 *for _, frame := range originalImage.Image {
-	 *    originalPalette := frame.Palette
-	 *    newPalette := make([]color.Color, len(frame.Palette))
-	 *}
-	 */
+	for frameIndex, frame := range image.Image {
+		originalPalette := frame.Palette
+		newPalette := make([]color.Color, len(frame.Palette))
 
-	/*
-	 *transformedImage := gif.GIF{
-	 *    Image: make([]*image.Paletted, 0, len(originalImage.Image)),
-	 *    Delay: make([]int, len(originalImage.Delay)),
-	 *    LoopCount: originalImage.LoopCount,
-	 *    Disposal: originalImage.Disposal,
-	 *    Config: originalImage.Config,
-	 *    BackgroundIndex: originalImage.BackgroundIndex,
-	 *}
-	 */
+		for pixelIndex, pixel := range originalPalette {
+			r, g, b, alpha := pixel.RGBA()
+
+			convertedPixel := colorful.Color{
+				float64(r) / 255,
+				float64(g) / 255,
+				float64(b) / 255,
+			}
+
+			blendedPixel,_ := blendAlpha(overlayColors[frameIndex], 0.5, convertedPixel, 1)
+			blendedR, blendedG, blendedB, _ := blendedPixel.RGBA()
+			newPalette[pixelIndex] = color.NRGBA{
+				uint8(blendedR),
+				uint8(blendedG),
+				uint8(blendedB),
+				255,
+			}
+		}
+
+		frame.Palette = newPalette
+	}
+
+	file, err = os.OpenFile("test-output.gif", os.O_RDWR | os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		os.Exit(1)
+	}
+
+	image.Config.ColorModel = nil
+	image.BackgroundIndex = 0
+
+	err = gif.EncodeAll(file, image)
+	if err != nil {
+		fmt.Println("Error encoding image: ", err)
+		os.Exit(1)
+	}
 }
