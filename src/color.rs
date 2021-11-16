41719f17bcaab145efe1384fc12003efac8c1ed5
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-11-15 23:55:02 -0500

    hcl gradient

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,5 @@
 use palette::rgb::LinSrgba;
-use palette::Gradient;
+use palette::{white_point, Gradient, Lch};
 use std::vec;
 
 pub struct GradientDescriptor {
@@ -70,13 +70,80 @@ impl GradientDescriptor {
     }
 }
 
+pub struct LchGradientDescriptor {
+    colors: vec::Vec<Lch<white_point::D65, f64>>,
+    positions: vec::Vec<f64>,
+    wrap: bool,
+}
+
+impl LchGradientDescriptor {
+    pub fn new(colors: vec::Vec<Lch<white_point::D65, f64>>, wrap: bool) -> LchGradientDescriptor {
+        let rng = if wrap && colors.len() > 1 {
+            0..colors.len() + 1
+        } else {
+            0..colors.len()
+        };
+        let length = colors.len();
+        return LchGradientDescriptor {
+            colors,
+            positions: rng.map(|i| (i as f64) / (length as f64)).collect(),
+            wrap,
+        };
+    }
+
+    pub fn generate(self, frame_count: usize) -> vec::Vec<Lch<white_point::D65, f64>> {
+        let mut gen = vec::Vec::<Lch<white_point::D65, f64>>::new();
+
+        // this only counts the number of colors forward, excluding color itself and the
+        // destination
+        let frames_per_color = frame_count / (self.colors.len() + 1);
+        let mut base_index = 0;
+        for (i, color) in self.colors.iter().enumerate() {
+            let src = color;
+            let dest = if i == self.colors.len() - 1 {
+                &self.colors[0]
+            } else {
+                &self.colors[i + 1]
+            };
+            let grad_iter = Gradient::new(vec![src.clone(), dest.clone()]);
+
+            let colors = grad_iter.take(frames_per_color + 2).collect::<Vec<_>>();
+            for j in 0..frames_per_color + 1 {
+                gen.push(colors[j]);
+            }
+        }
+
+        return gen;
+    }
+}
+
 mod tests {
     use palette::rgb::LinSrgba;
+    use palette::{Lch, FromColor};
 
     use crate::color;
 
     #[test]
-    fn test_new() {
+    fn test_generate_lch() {
+        let grad_desc = color::LchGradientDescriptor::new(
+            vec![
+                Lch::from_color(LinSrgba::new(0., 0., 0., 1.)),
+                Lch::from_color(LinSrgba::new(0.5, 0.5, 0.5, 1.)),
+                Lch::from_color(LinSrgba::new(1., 1., 1., 1.)),
+            ],
+            true,
+        );
+
+        let colors = grad_desc.generate(12);
+        assert_eq!(colors.len(), 12);
+
+        assert_eq!(colors[0].chroma, 0.0);
+        assert_eq!(colors[4].chroma, 0.0);
+        assert_eq!(colors[8].chroma, 0.0);
+    }
+
+    #[test]
+    fn test_generate_rgba() {
         let grad_desc = color::GradientDescriptor::new(
             vec![
                 LinSrgba::new(0., 0., 0., 1.),
