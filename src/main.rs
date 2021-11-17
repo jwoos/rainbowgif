bea9d5f47433ba98af6c1d08d7d0d9068ef65fa6
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-11-16 21:47:29 -0500

    reading and writing gif - there is a bit of an issue though

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use clap::{App, Arg};
+use std::fs::File;
 use std::vec;
+
+use clap::{App, Arg};
+use image::codecs::gif::{GifDecoder, GifEncoder};
+use image::AnimationDecoder;
 
 mod color;
 
@@ -13,7 +17,7 @@ fn main() -> Result<(), String> {
                 .help("The path to the input file"),
         )
         .arg(
-            Arg::with_name("OUPUT_FILE")
+            Arg::with_name("OUTPUT_FILE")
                 .required(true)
                 .help("The path to the output file"),
         )
@@ -42,5 +46,23 @@ fn main() -> Result<(), String> {
         }
     }
 
-    return Ok(());
+    let src_image_path = matches.value_of("INPUT_FILE").unwrap();
+    let src_image = match File::open(src_image_path) {
+        Ok(f) => f,
+        Err(e) => return Err(format!("{}: {}", e.to_string(), src_image_path)),
+    };
+
+    let mut decoder = GifDecoder::new(src_image).map_err(|e| e.to_string())?;
+    let src_frames = decoder
+        .into_frames()
+        .collect_frames()
+        .map_err(|e| format!("Unable to decode: {}", e.to_string()))?;
+
+    let dest_image_path = matches.value_of("OUTPUT_FILE").unwrap();
+    let mut dest_image = File::create(dest_image_path).map_err(|e| e.to_string())?;
+    let mut encoder = GifEncoder::new(dest_image);
+
+    return encoder
+        .encode_frames(src_frames.into_iter())
+        .map_err(|e| e.to_string());
 }
