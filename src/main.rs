3e5d72c72361ce0cae8d851bba2fec07273df871
Jun Woo Shin <shin.junwoo97@gmail.com>
2021-11-21 15:22:33 -0500

    use gif crate directly

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::fs::File;
 use std::vec;
 
 use clap::{App, Arg};
-use image::codecs::gif::{GifDecoder, GifEncoder};
-use image::AnimationDecoder;
+use gif;
 
 mod color;
 
@@ -52,17 +51,25 @@ fn main() -> Result<(), String> {
         Err(e) => return Err(format!("{}: {}", e.to_string(), src_image_path)),
     };
 
-    let mut decoder = GifDecoder::new(src_image).map_err(|e| e.to_string())?;
-    let src_frames = decoder
-        .into_frames()
-        .collect_frames()
-        .map_err(|e| format!("Unable to decode: {}", e.to_string()))?;
+    let mut decoder_options = gif::DecodeOptions::new();
+    decoder_options.set_color_output(gif::ColorOutput::RGBA);
+    let mut decoder = decoder_options.read_info(src_image).map_err(
+        |e| format!("Unable to open decoder: {}", e.to_string())
+        )?;
 
     let dest_image_path = matches.value_of("OUTPUT_FILE").unwrap();
     let mut dest_image = File::create(dest_image_path).map_err(|e| e.to_string())?;
-    let mut encoder = GifEncoder::new(dest_image);
+    let mut encoder = gif::Encoder::new(
+        &mut dest_image,
+        decoder.width(),
+        decoder.height(),
+        &decoder.global_palette().unwrap_or(&[])
+    ).map_err(|e| format!("Unable to open encoder: {}", e.to_string()))?;
+    encoder.set_repeat(gif::Repeat::Infinite).unwrap();
 
-    return encoder
-        .encode_frames(src_frames.into_iter())
-        .map_err(|e| e.to_string());
+    while let Some(frame) = decoder.read_next_frame().map_err(|e| format!("Unable to decode: {}", e.to_string()))? {
+        encoder.write_frame(&frame).map_err(|e| format!("Unable to encode: {}", e.to_string()))?;
+    }
+
+    return Ok(());
 }
